df90f96ebbbe3c087e5a42952ccc3bead47d7822
matiasarturr <arturmatias0505@gmail.com>
2026-04-17 00:35:21 -0300

    add P js, css e html

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector('.input-min').value)
     const max = Math.floor(document.querySelector('.input-max').value)
 
+    if (!min || !max || min > max) {
+        document.querySelector('.resultado').innerText = "Valores inválidos"
+        return
+    }
+
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-    alert(result)
+    document.querySelector('.resultado').innerText = result
 }
